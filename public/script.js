c2a6f8c6979db4c78a8f45429f87bf80fb43b4d4
megaton20 <128734956+megaton20@users.noreply.github.com>
2024-10-14 14:59:16 +0100

    ui1

--- public/script.js
+++ public/script.js
@@ -1,10 +1,54 @@
 // script.js
-
+// Handle scroll event to change navbar style
 window.addEventListener('scroll', function() {
   const navbar = document.getElementById('navbar');
+  const navLinks = document.querySelectorAll('.nav-link');
+  
   if (window.scrollY > 50) { // Adjust threshold as needed
-    navbar.classList.add('scrolled');
+      navbar.classList.add('scrolled');
+      navLinks.forEach(navLink => {
+        navLink.classList.add('text-light');
+      });
   } else {
-    navbar.classList.remove('scrolled');
+      navbar.classList.remove('scrolled');
+      navLinks.forEach(navLink => {
+        navLink.classList.remove('text-light');
+      });
   }
 });
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
